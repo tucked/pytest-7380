8e7e0779dcfaa0993fc23f67e5502923c1ae7b2b
David Tucker <dtucker@isilon.com>
2021-10-11 10:47:04 -0700

    Update repo link

--- repro.bash
+++ repro.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # https://github.com/pytest-dev/pytest/issues/7380
-# git clone https://gist.github.com/7bc58b91983f096ce69dcdc9ce8a9302.git
-# cd 7bc58b91983f096ce69dcdc9ce8a9302/
+# git clone https://github.com/tucked/pytest-7380.git
+# cd pytest-7380/
 python3 -m venv venv
 venv/bin/python -m pip install pytest
 venv/bin/pytest --foo="what ever" --bar=baz path/to/some/tests/
